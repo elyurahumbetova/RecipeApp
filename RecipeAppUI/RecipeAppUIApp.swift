81b91d1e871ca115d5cbee676fbcd170c5183916
Elyura <hhumbetovaa@gmail.com>
2026-03-09 19:01:12 +0400

    Initial Commit

---
+++ RecipeAppUI/RecipeAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppUIApp.swift
+//  RecipeAppUI
+//
+//  Created by Elyura on 09.03.26.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
