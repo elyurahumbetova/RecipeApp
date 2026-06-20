1d9bae97ba4cba21eb085a251763b24c76cac194
Elyura <hhumbetovaa@gmail.com>
2026-06-20 20:28:19 +0400

    detailedView created

---
+++ RecipeAppUI/View/DetailView1.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView1.swift
+//  RecipeAppUI
+//
+//  Created by Elyura on 20.06.26.
+//
+
+import SwiftUI
+
+struct DetailView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView1()
+}
